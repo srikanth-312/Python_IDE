b5bb677ca3ced4ba15e923d11525ed368f73e2d7
Srikanth Bhuvanagiri <107842504+srikanth-312@users.noreply.github.com>
2026-02-12 19:33:49 +0530

    Create main.py
    Sample python code

---
+++ main.py
@@ -0,0 +1,13 @@
+##python list 
+
+list1 = [1,2,3,4,5,6,7,8,9,10]
+even = []
+odd = []
+
+for i in list1: 
+  if i / 2 == 0:
+    even.append(i)
+  else:
+    odd.append(i)
+
+print("Completed for python code")
