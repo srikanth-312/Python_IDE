cfdb6e258ae4cdb65026e5777f46c356b500ce8f
Srikanth <srikanthbhuvanagiri1140@gmail.com>
2026-02-12 22:49:18 +0530

    Add new file and added logic change in main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ even = []
 odd = []
 
 for i in list1: 
-  if i / 2 == 0:
+  if i % 2 == 0:
     even.append(i)
   else:
     odd.append(i)
 
+print(even)
+print(odd)
 print("Completed for python code")
